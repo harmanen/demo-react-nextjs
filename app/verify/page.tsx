67951e11bbcf7b4b25be416a348ad638d49ed605
Harmanen <hurmanen@gmail.com>
2023-10-27 10:39:45 +0300

    Verify that Redux works

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,11 +1,14 @@
+"use client";
+
+import { selectCount } from "@/lib/redux";
+import { useSelector } from "react-redux";
+
 export default function VerifyPage() {
+  const count = useSelector(selectCount);
   return (
     <>
       <h1>Verify page</h1>
-      <p>
-        This page is intended to verify that Redux state is persisted across
-        page navigations.
-      </p>
+      <p>Count: {count}</p>
     </>
   );
 }
