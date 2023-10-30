b1c90154946472c482f900d5817d21b200567f1c
Harmanen <hurmanen@gmail.com>
2023-10-30 11:52:00 +0200

    Add Form UI components

--- app/components/Customer/Form.tsx
+++ app/components/Customer/Form.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import { TextField, Button } from "@mui/material";
 import styles from "./form.module.css";
 
 const Header = () => {
-  return <div className={styles.container}>form</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.text}>Customer Number</h2>
+      <div className={styles.input}>
+        <TextField fullWidth />
+      </div>
+      <div className={styles.button}>
+        <Button variant="contained">Continue</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
