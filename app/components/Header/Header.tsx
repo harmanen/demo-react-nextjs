df897fae18eb767753ebdf82696b719c2161af5c
Harmanen <hurmanen@gmail.com>
2023-10-30 16:18:58 +0200

    Display Order component only if customer exists

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,13 +1,15 @@
 "use client";
-
+import { useSelector, selectCustomer } from "@/lib/redux";
 import styles from "./header.module.css";
 import Order from "./Order";
 
 const Header = () => {
+  const customer = useSelector(selectCustomer);
+
   return (
     <div className={styles.container}>
       <h1>Oy Pulju AB</h1>
-      <Order />
+      {customer && <Order />}
     </div>
   );
 };
