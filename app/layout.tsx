3856850e7535f40e48e46657b583dfdf64c80e3e
Harmanen <hurmanen@gmail.com>
2023-10-30 10:02:43 +0200

    Define site container layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Providers } from "@/lib/providers";
 import "./styles/globals.css";
+import styles from "./styles/rootLayout.module.css";
 
 export default function RootLayout(props: React.PropsWithChildren) {
   return (
     <Providers>
-      <html lang="en">
-        <body>
-          <div>Root layout</div>
-          <main>{props.children}</main>
+      <html
+        lang="en"
+        className={styles.container}
+      >
+        <body className={styles.container}>
+          <div className={styles.siteWidth}>Root layout</div>
+          <main className={styles.siteWidth}>{props.children}</main>
         </body>
       </html>
     </Providers>
