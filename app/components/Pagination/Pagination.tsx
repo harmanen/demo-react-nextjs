32d1a3439b4d4766b093a205ee3d213899c2772e
Harmanen <hurmanen@gmail.com>
2023-10-31 10:10:19 +0200

    Move product data state to page, add page numbers

--- app/components/Pagination/Pagination.tsx
+++ app/components/Pagination/Pagination.tsx
@@ -2,42 +2,52 @@
 
 import PropTypes from "prop-types";
 import { Button } from "@mui/material";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import styles from "./pagination.module.css";
 
 const Pagination = (props: { props: { customer: number } }) => {
-  const [productsData, setProductsData] = useState({});
-
   useEffect(() => {
     // Set initial state if there are no products
-    if (Object.keys(productsData).length === 0) {
+    if (Object.keys(props.productsData).length === 0) {
       fetch(
         `https://bakery-3c739d39.digi.loikka.dev/v1/bakery/products?customerNumber=${props.customer}&skip=0&limit=6`,
       )
         .then((response) => response.json())
-        .then((data) => setProductsData(data));
+        .then((data) => props.setProductsData(data));
     }
-  }, [props.customer, productsData]);
+  });
 
   return (
-    <div className={styles.container}>
-      <div className={styles.buttonContainer}>
-        <Button
-          variant="contained"
-          style={{ marginRight: "1rem" }}
-        >
-          ⟪ Previous
-        </Button>
-        <Button variant="contained">Next ⟫</Button>
+    // Display pagination only if metadata exists
+    props.productsData?.metadata && (
+      <div className={styles.container}>
+        <div className={styles.buttonContainer}>
+          <Button
+            variant="contained"
+            style={{ marginRight: "1rem" }}
+          >
+            ⟪ Previous
+          </Button>
+          <Button variant="contained">Next ⟫</Button>
+        </div>
+        <div className={styles.page}>{`Page ${
+          1 +
+          props.productsData.metadata.skip / props.productsData.metadata.limit
+        } / ${
+          props.productsData.metadata.total / props.productsData.metadata.limit
+        }`}</div>
+        <div className={styles.amount}>
+          Total {props.productsData.metadata.total} products
+        </div>
       </div>
-      <div className={styles.page}>Page X/Y</div>
-      <div className={styles.amount}>Total XX products</div>
-    </div>
+    )
   );
 };
 
 Pagination.propTypes = {
   customer: PropTypes.string,
+  productsData: PropTypes.object,
+  setProductsData: PropTypes.func,
 };
 
 export default Pagination;
--- app/customer/[id]/page.tsx
+++ app/customer/[id]/page.tsx
@@ -1,12 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import Pagination from "../../components/Pagination/Pagination";
 
 export default function Page({ params }: { params: { id: string } }) {
+  const [productsData, setProductsData] = useState({});
+
   return (
     <div style={{ width: "100%" }}>
       <div style={{ width: "100%", height: "400px", border: "1px solid red" }}>
         Customer: {params.id} - Place cards here
       </div>
-      <Pagination customer={params.id} />
+      <Pagination
+        customer={params.id}
+        productsData={productsData}
+        setProductsData={setProductsData}
+      />
     </div>
   );
 }
