c2c31af07dcb92aba4570e64b49c3c64ed288917
Harmanen <hurmanen@gmail.com>
2023-10-31 09:39:08 +0200

    Fetch initial products data to component state

--- app/components/Pagination/Pagination.tsx
+++ app/components/Pagination/Pagination.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import PropTypes from "prop-types";
 import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
 import styles from "./pagination.module.css";
 
-const Pagination = () => {
+const Pagination = (props: { props: { customer: number } }) => {
+  const [productsData, setProductsData] = useState({});
+
+  useEffect(() => {
+    // Set initial state if there are no products
+    if (Object.keys(productsData).length === 0) {
+      fetch(
+        `https://bakery-3c739d39.digi.loikka.dev/v1/bakery/products?customerNumber=${props.customer}&skip=0&limit=6`,
+      )
+        .then((response) => response.json())
+        .then((data) => setProductsData(data));
+    }
+  }, [props.customer, productsData]);
+
   return (
     <div className={styles.container}>
       <div className={styles.buttonContainer}>
@@ -17,6 +34,10 @@ const Pagination = () => {
       <div className={styles.amount}>Total XX products</div>
     </div>
   );
+};
+
+Pagination.propTypes = {
+  customer: PropTypes.string,
 };
 
 export default Pagination;
--- app/customer/[id]/page.tsx
+++ app/customer/[id]/page.tsx
@@ -1,12 +1,12 @@
 import Pagination from "../../components/Pagination/Pagination";
 
-export default function Page({ params }: { params: { id: number } }) {
+export default function Page({ params }: { params: { id: string } }) {
   return (
     <div style={{ width: "100%" }}>
       <div style={{ width: "100%", height: "400px", border: "1px solid red" }}>
         Customer: {params.id} - Place cards here
       </div>
-      <Pagination />
+      <Pagination customer={params.id} />
     </div>
   );
 }
