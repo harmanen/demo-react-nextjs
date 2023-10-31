65bbaec1d9e4460ab414334d66b6fda5a683df03
Harmanen <hurmanen@gmail.com>
2023-10-31 09:17:53 +0200

    Add Pagination to the page

--- app/customer/[id]/page.tsx
+++ app/customer/[id]/page.tsx
@@ -1,3 +1,12 @@
+import Pagination from "../../components/Pagination/Pagination";
+
 export default function Page({ params }: { params: { id: number } }) {
-  return <div>Customer: {params.id}</div>;
+  return (
+    <div style={{ width: "100%" }}>
+      <div style={{ width: "100%", height: "400px", border: "1px solid red" }}>
+        Customer: {params.id} - Place cards here
+      </div>
+      <Pagination />
+    </div>
+  );
 }
