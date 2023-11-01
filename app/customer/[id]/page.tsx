a510554ddcecefb22a6c1e2fbd9c1c78c5bc28f5
Harmanen <hurmanen@gmail.com>
2023-11-01 15:01:48 +0200

    Populate Redux state if order has already been made

--- app/customer/[id]/page.tsx
+++ app/customer/[id]/page.tsx
@@ -1,11 +1,40 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {
+  useDispatch,
+  useSelector,
+  selectCustomer,
+  selectProducts,
+  productSlice,
+} from "@/lib/redux";
 import { Grid } from "@mui/material";
 import Pagination from "../../components/Pagination/Pagination";
 import Card from "../../components/Card/Card";
 
 export default function Page({ params }: { params: { id: string } }) {
+  // Populate Redux state with data if order has already been made
+  const dispatch = useDispatch();
+  const customer = useSelector(selectCustomer);
+  const selectedProducts = useSelector(selectProducts);
+
+  useEffect(() => {
+    if (
+      customer.data[0].status === "ordered" &&
+      selectedProducts.length === 0
+    ) {
+      customer.data[0].products.forEach((item) => {
+        dispatch(
+          productSlice.actions.addProduct({
+            product: item.id,
+            amount: item.amount,
+          }),
+        );
+      });
+    }
+  });
+
+  // Products to show on Grid
   const [productsData, setProductsData] = useState({});
 
   return (
