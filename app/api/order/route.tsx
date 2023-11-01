a01b8c0c967bb80b0be10aa9c888b21c6e4b7aed
Harmanen <hurmanen@gmail.com>
2023-11-01 14:13:00 +0200

    Create route /api/order

---
+++ app/api/order/route.tsx
@@ -0,0 +1,29 @@
+import type { NextApiRequest } from "next";
+import { BASE_URL } from "../../constants";
+
+export async function POST(req: NextApiRequest) {
+  const body = await req.json();
+
+  const response = await fetch(`${BASE_URL}v1/bakry`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (response.ok) {
+    return response;
+  } else {
+    return new Response(
+      JSON.stringify({
+        error: "Order placement failed!",
+        status: 500,
+      }),
+      {
+        status: 500,
+        statusText: "Order placement failed!",
+      },
+    );
+  }
+}
