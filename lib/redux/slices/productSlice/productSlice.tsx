645908e62e6853b66cbe13c9ab1ac9d1d0cff8e3
Harmanen <hurmanen@gmail.com>
2023-11-01 11:27:16 +0200

    Fix a bug and rounding to product reducers

--- lib/redux/slices/productSlice/productSlice.tsx
+++ lib/redux/slices/productSlice/productSlice.tsx
@@ -13,16 +13,16 @@ export const productSlice = createSlice({
       return {
         ...state,
         selectedProducts: [...state.selectedProducts, action.payload.product],
-        total: state.total + action.payload.amount,
+        total: Math.round((state.total + action.payload.amount) * 100) / 100, // 2 decimals
       };
     },
     removeProduct: (state, action: PayloadAction) => {
       return {
         ...state,
         selectedProducts: state.selectedProducts.filter(
-          (product) => product !== action.payload.products,
+          (product) => product !== action.payload.product,
         ),
-        total: state.total - action.payload.amount,
+        total: Math.round((state.total - action.payload.amount) * 100) / 100, // 2 decimals
       };
     },
   },
